9d1e0a4236b4413654ddedc2d4e6d02f385898f0
Surnaya <surnaya.m@gmail.com>
2024-05-06 20:13:28 +0400

    создание представлений "главная" и "обо мне"

--- homeproject/myapp/views.py
+++ homeproject/myapp/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    html = """
+    <h1>Добро пожаловать на мой первый Django сайт!</h1>
+    <p>Здесь пока ничего нет .....</p>
+    """
+    return HttpResponse(html)
+
+
+def about(request):
+    html = """
+    <h1>Обо мне</h1>
+    <p>Привет! Меня зовут Рита. Это мой первый проект на Django.</p>
+    """
+    return HttpResponse(html)
