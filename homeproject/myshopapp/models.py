e729d98eedf500d8bc70e48a75091928fd88e70d
Surnaya <surnaya.m@gmail.com>
2024-05-12 00:05:06 +0400

    созданы модели Клиент, Товар, Заказ

--- homeproject/myshopapp/models.py
+++ homeproject/myshopapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    registration_date = models.DateTimeField(default=timezone.now)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    count = models.IntegerField()
+    added_date = models.DateTimeField(default=timezone.now)
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(default=timezone.now)
